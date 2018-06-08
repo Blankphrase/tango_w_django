f4b7748448e5e1ae22f900779e08be690207a02f
SimasJan <simjanusas@gmail.com>
2018-06-08 13:40:54 +0100

    likes, views added to category class in rango/models.py

--- tango_with_django_project/rango/models.py
+++ tango_with_django_project/rango/models.py
@@ -2,6 +2,8 @@ from django.db import models
 
 class Category(models.Model):
     name = models.CharField(max_length=128, unique=True, null=False) # field's value must be unique throughout the underlying databse table that is mapped. Every category must be unique, cannot be NULL.
+    views = models.IntegerField(default=0)
+    likes = models.IntegerField(default=0)
 
     class Meta:
         verbose_name_plural = 'Categories'
